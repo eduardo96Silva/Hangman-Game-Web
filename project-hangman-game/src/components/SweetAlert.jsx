9a14785a268dfe75b2aa61834ee4f47f60381e81
eduardo96Silva <eduardo9631s@gmail.com1>
2024-06-25 20:57:13 -0300

    UPDATE: Animation in results (game over / game win)

---
+++ project-hangman-game/src/components/SweetAlert.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import Swal from 'sweetalert2';
+
+export function Alert(icon, title, text, timer, showConfirmButton) {
+
+    if(title === 'Game Over'){
+        return (
+            Swal.fire({
+                width: 300,
+                background: '#fff',
+                color: 'rgba(200,0,0,1)',
+                position: 'center',
+                icon: icon,
+                title: title,
+                text: text,
+                showConfirmButton: showConfirmButton,
+                timer: timer,
+                timerProgressBar: true,
+                backdrop: `
+                    rgba(255,0,0,0.15)
+                    url("/imgs/icon-gameover.gif")
+                    left bottom
+                    no-repeat
+                `,
+            })
+        )
+
+    } else if (title === 'Parabéns'){
+        return (
+            Swal.fire({
+                width: 300,             
+                background: '#fff',
+                color: 'rgba(0,100,0,1)',
+                position: 'center',
+                icon: icon,
+                title: title,
+                text: text,
+                showConfirmButton: showConfirmButton,
+                confirmButtonColor: 'rgba(0,100,0,1)',
+                timer: timer,
+                timerProgressBar: true,
+                backdrop: `
+                    rgba(0,234,0,0.20)
+                    url("/imgs/icon-victory.gif")
+                    left bottom
+                    no-repeat
+                `,
+            
+            })
+        )
+
+    } else {
+        return (
+            Swal.fire({
+                position: 'center',
+                icon: icon,
+                title: title,
+                text: text,
+                showConfirmButton: showConfirmButton,
+                timer: timer
+            })
+        )
+    }
+
+    
+}
--- project-hangman-game/src/pages/Gameview.jsx
+++ project-hangman-game/src/pages/Gameview.jsx
@@ -1,8 +1,6 @@
-// Styles
+
 import style from '../css/GameView.module.css'
 import '../css/index.css'
-
-// IMGS
 import stage1 from '/imgs/assets/stage-1.png'
 import stage2 from '/imgs/assets/stage-2.png'
 import stage3 from '/imgs/assets/stage-3.png'
@@ -10,59 +8,105 @@ import stage4 from '/imgs/assets/stage-4.png'
 import stage5 from '/imgs/assets/stage-5.png'
 import stage6 from '/imgs/assets/stage-6.png'
 import stage7 from '/imgs/assets/stage-7.png'
-
-// Hooks
 import { useState } from 'react'
-
-
-// Data
-import palavras from '../data/palavras'
 import { useEffect } from 'react'
+import palavras from '../data/palavras'
+import { Alert } from '../components/SweetAlert'
 
 function Gameview() {
 
     let stages = [stage1, stage2, stage3, stage4, stage5, stage6, stage7]
     const [gameStarted, setGameStarted] = useState(false);
+    const [buttonRestart, setButtonRestart] = useState(false);
     const [stageAtual, setStageAtual] = useState(0)
     const [palavraSelecionada, setPalavraSelecionada] = useState(null);
     const [qtdLetras, setQtdLetras] = useState(0);
     const [letrasCorretas, setLetrasCorretas] = useState([]);
     const [vidasRestantes, setVidasRestantes] = useState(6);
 
+    // Iniciar o Jogo
     const startGame = () => {
         setGameStarted(true);
-        const randomIndex = Math.floor(Math.random() * palavras.length);
-        setPalavraSelecionada(palavras[randomIndex]);
-        setQtdLetras(palavras[randomIndex].palavra.length);
+        const randomIndex = Math.floor(Math.random() * palavras.length); // Pegando uma palavra aleatoria da lista de palavras
+        setPalavraSelecionada(palavras[randomIndex]); // guardando a palavra 
+        setQtdLetras(palavras[randomIndex].palavra.length); // guardando a quantidade de letras que a palavra possui
+        setLetrasCorretas([]);
+        setVidasRestantes(6);
+        setStageAtual(0);
+        setButtonRestart(false)
+        document.querySelector('.area_palavra').style.display = 'flex';
+        document.querySelector('.area_keyboard').style.display = 'flex'; 
+    };
+
+    // Reiniciar o Jogo
+    const restartGame = () => {
+        setStageAtual(0);
+        setPalavraSelecionada(null);
+        setQtdLetras(0);
+        setLetrasCorretas([]);
+        setVidasRestantes(6);
+        startGame()
+        document.querySelectorAll('.area_keyboard button').forEach(element => {
+            element.disabled = false; // Mostrando novamente todas as letras do teclado
+            element.classList.remove('buttonDisabled'); // Mostrando novamente todas as letras do teclado
+        });
     };
 
 
+
+    // Testando a letra informada pelo usuário
     const testarLetra = (event) => {
-        console.log(event.target.style.backgroundColor)
-        const letra = event.target.textContent.toLowerCase();
-        if (palavraSelecionada.palavra.includes(letra)) {
-            setLetrasCorretas([...letrasCorretas, letra]);
-            event.target.style.display= 'none';
+        const letra = event.target.textContent.toLowerCase(); // Pegando a letra selecionada e transformando em minuscula
+        if (palavraSelecionada.palavra.includes(letra)) { // Condição caso haja a letra na palavra
+            setLetrasCorretas([...letrasCorretas, letra]); // atribuindo a letra na lista de letras corretas
+            event.target.disabled = true;
+            event.target.classList.add('buttonDisabled');
 
-        } else {
-            setStageAtual(stageAtual+1)
-            setVidasRestantes(vidasRestantes-1)
-            event.target.style.display= 'none';
-
+        } else { // Condição caso NÃO haja a letra na palavra
+            setStageAtual(stageAtual + 1) // Aumentando o numero de erro
+            setVidasRestantes(vidasRestantes - 1)  // Definindo as vidas restantes
+            event.target.disabled = true;
+            event.target.classList.add('buttonDisabled');
         }
     };
 
-    useEffect(() => {
-        if(vidasRestantes == 0){
-            document.querySelector('.area_palavra').style.display = 'none';
-            document.querySelector('.area_keyboard').style.display = 'none';
-        }
-    }, [vidasRestantes]);
 
+    // useEffect para monitorar a renderização das vidas restantes
+    useEffect(() => {
+        var qtdLetrasOcultas = 0;
+        if(document.querySelector('.area_palavra')){
+            const spans = document.querySelector('.area_palavra').children
+            for (var i = 0; i < spans.length; i++){
+                if (spans[i].innerHTML == '&nbsp;'){
+                    qtdLetrasOcultas++;
+                }
+            }
+            if(qtdLetrasOcultas == 0){
+                document.querySelector('.area_palavra').style.display = 'none'; // Ocultando palavra
+                document.querySelector('.area_keyboard').style.display = 'none'; // Ocultando o Keyboard
+                setButtonRestart(true)
+                Alert(false, 'Parabéns', 'Você acertou todas as Letras !', 7000, true)
+            }
+        }
+
+        if (vidasRestantes == 0) { // Condição caso esgote todas as vidas
+            document.querySelector('.area_palavra').style.display = 'none'; // Ocultando palavra
+            document.querySelector('.area_keyboard').style.display = 'none'; // Ocultando o Keyboard
+            setButtonRestart(true)
+            Alert(false, 'Game Over', 'Você não completou a palavra.', 7000, true)
+            
+        } 
+        
+    }, [vidasRestantes, letrasCorretas]);
+
+
+    // Criando a quantidade de spans de acordo com a quantidade de letras que a palavra possui
+    // E após cada acerto, a letra fica visivel
     const spans = Array.from({ length: qtdLetras }).map((_, index) => {
         const letra = palavraSelecionada.palavra[index];
         return <span key={index}>{letrasCorretas.includes(letra) ? letra : <>&nbsp;</>}</span>;
     });
+
 
     return (
         <>
@@ -71,7 +115,7 @@ function Gameview() {
                     !gameStarted ? (
                         <>
                             <div id={style.lobby}>
-                                <button onClick={startGame}>Começar jogo</button>
+                                <button className='btnStartGame' onClick={startGame}>Começar jogo</button>
                             </div>
                         </>
                     ) : (
@@ -80,51 +124,58 @@ function Gameview() {
                                 <section id={style.tableGame}>
 
                                     <div id={style.descript}>
-                                        <span>Categoria: <b>{palavraSelecionada.categoria}</b></span>   
+                                        <span>Categoria: <b>{palavraSelecionada.categoria}</b></span>
                                         <span>Quantidade de Letras: <b>{palavraSelecionada.palavra.length}</b></span>
-                                        <span>Vidas restantes: <b style={{color:'red', fontSize: '1.4em'}}>{vidasRestantes}</b></span>
+                                        <span>Vidas restantes: <b style={{ color: 'red', fontSize: '1.4em' }}>{vidasRestantes}</b></span>
                                     </div>
 
                                     <div id={style.areaForca}>
-                                        <img id='forca' src={stages[stageAtual]} alt="forca" style={{ width: '180px', marginTop: '30px', marginBottom: '-30px', transition: 'all 0.8s'}} />
+                                        <img id='forca' src={stages[stageAtual]} alt="forca" style={{ width: '180px', marginTop: '30px', marginBottom: '-30px', transition: 'all 0.8s' }} />
                                         <div id={style.palavra} className='area_palavra'>
                                             {spans}
                                         </div>
                                     </div>
-
+                                    {
+                                        buttonRestart ? (
+                                            <button className='btnStartGame' onClick={restartGame} display={buttonRestart}>Jogar novamente</button>
+                                        ) : (
+                                            <></>
+                                        )
+                                    }
+                                    
                                     <div id={style.keyboard} className='area_keyboard'>
                                         <div>
-                                            <span onClick={testarLetra}>Q</span>
-                                            <span onClick={testarLetra}>W</span>
-                                            <span onClick={testarLetra}>E</span>
-                                            <span onClick={testarLetra}>R</span>
-                                            <span onClick={testarLetra}>T</span>
-                                            <span onClick={testarLetra}>Y</span>
-                                            <span onClick={testarLetra}>U</span>
-                                            <span onClick={testarLetra}>I</span>
-                                            <span onClick={testarLetra}>O</span>
-                                            <span onClick={testarLetra}>P</span>
+                                            <button onClick={testarLetra}>Q</button>
+                                            <button onClick={testarLetra}>W</button>
+                                            <button onClick={testarLetra}>E</button>
+                                            <button onClick={testarLetra}>R</button>
+                                            <button onClick={testarLetra}>T</button>
+                                            <button onClick={testarLetra}>Y</button>
+                                            <button onClick={testarLetra}>U</button>
+                                            <button onClick={testarLetra}>I</button>
+                                            <button onClick={testarLetra}>O</button>
+                                            <button onClick={testarLetra}>P</button>
                                         </div>
                                         <div>
-                                            <span onClick={testarLetra}>A</span>
-                                            <span onClick={testarLetra}>S</span>
-                                            <span onClick={testarLetra}>D</span>
-                                            <span onClick={testarLetra}>F</span>
-                                            <span onClick={testarLetra}>G</span>
-                                            <span onClick={testarLetra}>H</span>
-                                            <span onClick={testarLetra}>J</span>
-                                            <span onClick={testarLetra}>K</span>
-                                            <span onClick={testarLetra}>L</span>
+                                            <button onClick={testarLetra}>A</button>
+                                            <button onClick={testarLetra}>S</button>
+                                            <button onClick={testarLetra}>D</button>
+                                            <button onClick={testarLetra}>F</button>
+                                            <button onClick={testarLetra}>G</button>
+                                            <button onClick={testarLetra}>H</button>
+                                            <button onClick={testarLetra}>J</button>
+                                            <button onClick={testarLetra}>K</button>
+                                            <button onClick={testarLetra}>L</button>
                                         </div>
                                         <div>
-                                            <span onClick={testarLetra}>Z</span>
-                                            <span onClick={testarLetra}>X</span>
-                                            <span onClick={testarLetra}>C</span>
-                                            <span onClick={testarLetra}>V</span>
-                                            <span onClick={testarLetra}>B</span>
-                                            <span onClick={testarLetra}>N</span>
-                                            <span onClick={testarLetra}>M</span>
-                                            <span onClick={testarLetra}>Ç</span>
+                                            <button onClick={testarLetra}>Z</button>
+                                            <button onClick={testarLetra}>X</button>
+                                            <button onClick={testarLetra}>C</button>
+                                            <button onClick={testarLetra}>V</button>
+                                            <button onClick={testarLetra}>B</button>
+                                            <button onClick={testarLetra}>N</button>
+                                            <button onClick={testarLetra}>M</button>
+                                            <button onClick={testarLetra}>Ç</button>
                                         </div>
                                     </div>
                                 </section>
@@ -137,4 +188,4 @@ function Gameview() {
         </>
     )
 }
-            export default Gameview;
+export default Gameview;
