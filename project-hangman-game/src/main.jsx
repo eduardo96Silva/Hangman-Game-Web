43a43e8904cd723de7563695d8fde55ffc0c7dbd
eduardo96Silva <eduardo9631s@gmail.com1>
2024-07-12 13:43:41 -0300

    UPDATE: rename dirs and rebuild router configs

--- project-hangman-game/src/main.jsx
+++ project-hangman-game/src/main.jsx
@@ -2,42 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// Pages
-import App from './routes/App.jsx';
-import GameView from './routes/Gameview.jsx';
-import Scoreboard from './routes/Scoreboard.jsx';
-import Login from './routes/Login.jsx';
-import Cadastro from './routes/Cadastro.jsx';
-import NoPage from './routes/NoPage.jsx';
-import Perfil from './routes/Perfil.jsx';
-
-// Configs Router
-import { createBrowserRouter, Route, RouterProvider, Navigate, Outlet } from 'react-router-dom';
-import { AuthProvider, useAuth } from './contexts/authContext.jsx';
-
-const PrivateRoute = ({ element: Element, pathAlternate, ...rest }) => {
-  const { currentUser } = useAuth();
-
-  return currentUser ? <Element {...rest} /> : <Navigate to={pathAlternate} />;
-};
-
-const routerConfig = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <NoPage />,
-    children: [
-      { path: "/", element: <GameView /> },
-      { path: "/Login", element: <Login /> },
-      { path: "/Cadastro", element: <Cadastro /> },
-      {
-        path: "/Perfil",
-        element: <PrivateRoute element={Perfil} pathAlternate="/Login" />
-      },
-      { path: "/Scoreboard", element: <Scoreboard /> }
-    ]
-  }
-]);
+import { routerConfig } from './router/routerConfig.jsx';
+import { RouterProvider } from 'react-router-dom';
+import { AuthProvider } from './contexts/authContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ project-hangman-game/src/router/routerConfig.jsx
@@ -0,0 +1,35 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+// Pages
+import App from '../pages/App.jsx';
+import GameView from '../pages/Gameview.jsx';
+import Scoreboard from '../pages/Scoreboard.jsx';
+import Login from '../pages/Login.jsx';
+import Cadastro from '../pages/Cadastro.jsx';
+import NoPage from '../pages/NoPage.jsx';
+import Perfil from '../pages/Perfil.jsx';
+
+import { useAuth } from '../contexts/authContext.jsx';
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ element: Element, pathAlternate, ...rest }) => {
+    const { currentUser } = useAuth();
+
+    return currentUser ? <Element {...rest} /> : <Navigate to={pathAlternate} />;
+};
+
+
+export const routerConfig = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <NoPage />,
+        children: [
+            { path: "/", element: <GameView /> },
+            { path: "/Login", element: <Login /> },
+            { path: "/Cadastro", element: <Cadastro /> },
+            { path: "/Perfil", element: <PrivateRoute element={Perfil} pathAlternate="/Login" />},
+            { path: "/Scoreboard", element: <Scoreboard /> }
+        ]
+    }
+]);
